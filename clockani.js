0bf2eb153f8337ff3cbf26fb3c453800147123ef
acadhrix2004 <163326329+Webtechcode1122@users.noreply.github.com>
2024-07-30 06:39:27 +0530

    Add files via upload

---
+++ clockani.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const parent = document.querySelector('.page1');
+    const child = document.querySelector('.page1 h1');
+    const video = document.querySelector('.video');
+
+    child.addEventListener('mouseover', () => {
+        video.style.opacity = 1; // Show the video
+        video.play(); // Play the video
+    });
+
+    child.addEventListener('mouseout', () => {
+        video.style.opacity = 0; // Hide the video
+        video.pause(); // Pause the video
+        video.currentTime = 0; // Reset the video to the beginning
+    });
+});
