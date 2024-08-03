7831f7000b6b4bb0781cf5a2b107d4c2ac766413
acadhrix2004 <163326329+Webtechcode1122@users.noreply.github.com>
2024-08-03 21:17:14 +0530

    Add files via upload

--- clockani.js
+++ clockani.js
@@ -1,16 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const parent = document.querySelector('.page1');
-    const child = document.querySelector('.page1 h1');
-    const video = document.querySelector('.page1 video');
 
-    child.addEventListener('mouseover', () => {
-        video.style.opacity = 1; // Show the video
-        video.play(); // Play the video
-    });
+gsap.from(".page2 h1",{
+ y:300,
+ delay:1,
+ opacity:0,
+ duration:2,    
+ scrollTrigger:".page2",
+})
+gsap.from(".page2 h3",{
+    y:300,
+    delay:1,
+    opacity:0,
+    duration:2, 
+    scrollTrigger:".page2",   
+   })
+   gsap.from(".page2 a",{
+    y:300,
+    delay:1,
+    opacity:0,
+    duration:2,   
+    scrollTrigger:".page2", 
+   })
+   gsap.from(".page3 h1",{
+    y:300,
+    delay:1,
+    opacity:0,
+    duration:2,    
+    scrollTrigger:".page3",
+   })
+   gsap.from(".page3 h3",{
+       y:300,
+       delay:1,
+       opacity:0,
+       duration:2, 
+       scrollTrigger:".page3",   
+      })
+      gsap.from(".page3 a",{
+       y:300,
+       delay:1,
+       opacity:0,
+       duration:2,   
+       scrollTrigger:".page3", 
+      })
 
-    child.addEventListener('mouseout', () => {
-        video.style.opacity = 0; // Hide the video
-        video.pause(); // Pause the video
-        video.currentTime = 0; // Reset the video to the beginning
-    });
-});
